05b9566fc556ac7d55c43d2d1a55727be5e97cb0
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-04-12 22:05:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socketio")(http);
+const io = require("socket.io")(http);
+
+const port = 4000;
+
+io.on("connection", (socket) => {
+  socket.on("message", ({ name, message }) => {
+    io.emit("message", { name, message });
+  });
+});
+
+http.listen(port, () => {
+  console.log("listening on", port);
+});
