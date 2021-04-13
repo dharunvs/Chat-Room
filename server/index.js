c08e04e82b4966608917c6c18f54654e63bd9164
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-04-13 12:26:23 +0530

    Updates

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
 
 const port = 4000;
-
 io.on("connection", (socket) => {
   socket.on("message", ({ name, message }) => {
     io.emit("message", { name, message });
   });
 });
 
-http.listen(port, () => {
-  console.log("listening on", port);
+http.listen(port, function () {
+  console.log("listening on port " + port);
 });
--- web/src/App.js
+++ web/src/App.js
@@ -1,54 +1,77 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
-import TextField from "@material-ui/core/TextField";
 import "./App.css";
 
-const socket = io.connect("http://localhost:4000");
-
 function App() {
-  const [state, setState] = useState({ message: "", name: "" });
+  const [state, setState] = useState({ message: "", name: "NAME" });
   const [chat, setChat] = useState([]);
 
-  function onTextChange(e) {}
+  const socketRef = useRef();
+
+  useEffect(() => {
+    socketRef.current = io.connect("http://localhost:4000");
+    socketRef.current.on("message", ({ name, message }) => {
+      setChat([...chat, { name, message }]);
+    });
+    return () => socketRef.current.disconnect();
+  }, [chat]);
+
+  const onTextChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const onMessageSubmit = (e) => {
+    const { name, message } = state;
+    socketRef.current.emit("message", { name, message });
+    e.preventDefault();
+    setState({ message: "", name });
+  };
 
   const renderChat = () => {
-    return chat.map(({ name, message }, index) => {
+    return chat.map(({ name, message }, index) => (
       <div key={index}>
-        <h3>
-          {name}: <span>{message}</span>
-        </h3>
-      </div>;
-    });
+        <div className="chat-element">
+          <div className="chat-element-name">{name}</div>
+          <div className="chat-element-message">{message}</div>
+        </div>
+      </div>
+    ));
   };
 
   return (
-    <div className="App">
-      <form onSubmit>
-        <h1>Messenger</h1>
+    <div className="card">
+      <div className="render-chat">
+        <h1>Chat Log</h1>
+        <div className="chat-window">{renderChat()}</div>
+      </div>
+      <form onSubmit={onMessageSubmit}>
         <div className="name-field">
-          <TextField
+          <input
+            className="name-field"
             name="name"
             onChange={(e) => onTextChange(e)}
             value={state.name}
+            onKeyUp={() => {
+              state.name = state.name.toUpperCase();
+            }}
             label="Name"
+            placeholder="Name"
           />
         </div>
         <div>
-          <TextField
+          <input
+            className="message-field"
             name="message"
             onChange={(e) => onTextChange(e)}
             value={state.message}
             id="outlined-multiline-static"
             variant="outlined"
             label="Message"
+            placeholder="Message"
           />
         </div>
-        <button>Send Message</button>
+        <button>Send</button>
       </form>
-      <div className="render-chat">
-        <h1>Chat Log</h1>
-        {renderChat()}
-      </div>
     </div>
   );
 }
